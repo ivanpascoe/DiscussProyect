89b29a36dbc8a88d92d97f23aae8ad111d3759f0
Ivan Pascoe <sinh.rivera@gmail.com>
2023-03-08 13:59:24 -0600

    new comment rendered via event.coment

--- discuss/assets/js/user_socket.js
+++ discuss/assets/js/user_socket.js
@@ -79,8 +79,8 @@ function renderComments(comments) {
   document.querySelector('.collection').innerHTML = renderedComments.join('');
 }
 
-function renderComment(comment) {
-  const renderedComment = commentTemplate(comment);
+function renderComment(event) {
+  const renderedComment = commentTemplate(event.comment);
   document.querySelector('.collection').innerHTML += renderedComment;
 }
 
