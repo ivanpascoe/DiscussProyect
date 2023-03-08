f12bf0635f9a28762272abbc5071ca6ad011b858
Ivan Pascoe <sinh.rivera@gmail.com>
2023-03-08 11:03:52 -0600

    comment list fetched and rendered

--- discuss/assets/js/user_socket.js
+++ discuss/assets/js/user_socket.js
@@ -59,14 +59,24 @@ socket.connect()
 const createSocket = (topicId) => {
   let channel = socket.channel(`comments:${topicId}`, {})  //aqui estamos usando un poco de ES6 javascript
   channel.join()
-    .receive("ok", resp => { console.log("Joined successfully", resp) })
+    .receive("ok", resp => { renderComments(resp.comments); })
     .receive("error", resp => { console.log("Unable to join", resp) });
   document.querySelector('button').addEventListener('click', () => {
     const content = document.querySelector('textarea').value;
 
-    channel.push('comment:add', { content: content});
+    channel.push('comment:add', {content: content});
   });
 }
 
+function renderComments(comments) {
+  const renderedComments = comments.map(comment => {
+    return `
+      <li class="collection-item">
+        ${comment.content}
+      </li>
+      `;
+  });
+  document.querySelector('.collection').innerHTML = renderedComments.join('');
+}
 
 window.createSocket = createSocket;
