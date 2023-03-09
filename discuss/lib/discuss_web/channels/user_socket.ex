f1ac524afc914bebf588b98ba74f2537fbc06590
Ivan Pascoe <sinh.rivera@gmail.com>
2023-03-09 10:25:16 -0600

    user token generated and visible

--- discuss/lib/discuss_web/channels/user_socket.ex
+++ discuss/lib/discuss_web/channels/user_socket.ex
@@ -24,10 +24,12 @@ defmodule DiscussWeb.UserSocket do
   # performing token verification on connect.
   @impl true
   def connect(%{"token" => token}, socket, _connect_info) do
+    IO.inspect(token)
+    IO.puts("----Token-----")
       # max_age: 1209600 is equivalent to two weeks in seconds
-    case Phoenix.Token.verify(socket, "user socket", token, max_age: 1_209_600) do
+    case Phoenix.Token.verify(socket, "key", token, max_age: 1_209_600) do
         {:ok, user_id} ->
-          {:ok, assign(socket, :user, user_id)}
+          {:ok, assign(socket, :user_id, user_id)}
 
         {:error, reason} ->
           :error
